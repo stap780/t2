12d5da14b26995ba7fffb811221ac93de7f29154
stap780 <panaet80@mail.ru>
2025-08-08 19:20:13 +0300

    update initializer

--- config/initializers/mission_control_jobs.rb
+++
@@ -1,7 +0,0 @@
-# Configure Mission Control Jobs with authentication
-MissionControl::Jobs.base_controller_class = "AuthenticatedJobsController"
-
-Rails.application.configure do
-  # Mission Control Jobs will automatically detect Solid Queue
-  # Using custom base controller for authentication
-end
