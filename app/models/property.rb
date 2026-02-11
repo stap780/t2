e311c066d699f84d9adb340e1056e069a19ea6ba
stap780 <panaet80@mail.ru>
2026-02-11 14:41:18 +0300

    update default feature order

--- app/models/property.rb
+++ app/models/property.rb
@@ -5,7 +5,7 @@ class Property < ApplicationRecord
   has_many :products, -> { where(features: { featureable_type: 'Product' }) }, through: :features, source: :featureable, source_type: 'Product'
   has_many :detal_features, -> { where(featureable_type: 'Detal') }, class_name: 'Feature'
   has_many :detals, through: :detal_features, source: :featureable, source_type: 'Detal'
-  has_many :characteristics, dependent: :destroy
+  has_many :characteristics, -> { order(title: :asc) }, dependent: :destroy
   accepts_nested_attributes_for :characteristics, allow_destroy: true, reject_if: :all_blank
 
   validates :title, presence: true, uniqueness: true
