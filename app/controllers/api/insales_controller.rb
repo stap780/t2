077951bd1e050ae56c0e80ca17c68d230008c3d3
stap780 <panaet80@mail.ru>
2026-03-08 13:14:12 +0300

    add moysklad webhook

--- app/controllers/api/insales_controller.rb
+++ app/controllers/api/insales_controller.rb
@@ -1,4 +1,4 @@
-class Api::Webhooks::InsalesController < ApplicationController
+class Api::InsalesController < ApplicationController
   skip_before_action :require_authentication
   skip_before_action :verify_authenticity_token
 
