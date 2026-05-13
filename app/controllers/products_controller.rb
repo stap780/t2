44f34deff0c1ea36c64d85551169c084ac62ab30
stap780 <panaet80@mail.ru>
2026-05-13 12:39:18 +0300

    add bulk feature set to product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -221,6 +221,14 @@ class ProductsController < ApplicationController
   def bulk_features_edit
     if params[:product_ids]
       @products = Product.where(id: params[:product_ids]).includes(:features) #.order(:id)
+      template = @products.first
+      @bulk_features_display_product = Product.new
+      template&.features&.each do |f|
+        @bulk_features_display_product.features.build(
+          property_id: f.property_id,
+          characteristic_id: f.characteristic_id
+        )
+      end
       respond_to do |format|
         format.turbo_stream
       end
